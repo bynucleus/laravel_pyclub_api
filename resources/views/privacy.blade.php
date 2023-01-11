8d746190299f0108a6a4eaa738b4d846b950635e
sminth <emmanuelmalan225@gmail.com>
2023-01-11 15:18:57 +0000

    maj

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Politique de confidentialité - MyClub</title>
+  </head>
+  <body>
+    <h1>Politique de confidentialité</h1>
+    <p>
+      Les règles de confidentialité suivantes s'appliquent à l'application MyClub :
+    </p>
+    <ol>
+      <li>
+        Nous collectons des informations personnelles telles que votre nom, votre adresse e-mail, votre niveau et le club souhaité uniquement dans le but de vous inscrire à un club et de vous tenir informé de vos activités au sein du club.
+      </li>
+      <li>
+        Nous ne partageons pas ces informations personnelles avec des tierces parties sauf si cela est nécessaire pour fournir des services liés à l'application, tels que l'envoi de notifications ou l'accès à des ressources partagées.
+      </li>
+      <li>
+        Nous prenons des mesures de sécurité appropriées pour protéger vos informations personnelles contre la perte, l'utilisation abusive, l'accès non autorisé, la divulgation, la modification ou la destruction.
+      </li>
+      <li>
+        Nous nous conformons aux réglementations applicables en matière de protection de la vie privée et de protection des données.
+      </li>
+      <li>
+        Nous nous engageons à être transparents quant à la manière dont nous traitons vos informations personnelles et à vous donner un accès et un contrôle sur ces informations.
+      </li>
+      <li>
+        Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité, n'hésitez pas à nous contacter.
+      </li>
+    </ol>
+    <p>
+      MyClub est fortement conscient de la nécessité de protéger les données personnelles et la vie privée de ses utilisateurs. Nous nous engageons à respecter les normes élevées de confidentialité pour protéger les données de nos utilisateurs.
+    </p>
+    <footer>
+        <p>Copyright &copy; MyClub 2023</p>
+        <p>
+          Contact : <a href="mailto:virtus225one@gmail.com">virtus225one@gmail.com</a>
+        </p>
+      </footer>
+  </body>
+  <style>
+    body {
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 30px;
+}
+
+h1 {
+    font-size: 36px;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+ol {
+    margin: 0 0 20px 30px;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
+
+a {
+    color: #0088cc;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+footer {
+    background-color: #f5f5f5;
+    padding: 20px;
+    text-align: center;
+    font-size: 12px;
+    }
+footer p{
+    margin: 0;
+}
+
+  </style>
+</html>
